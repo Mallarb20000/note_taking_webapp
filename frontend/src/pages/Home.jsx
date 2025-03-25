d423f62003221137dbb28320d765e3103587a777
Rohit Malla <P2779735@my365.dmu.ac.uk>
2025-03-25 04:13:55 +0000

    Add Create Note Function in Home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -20,16 +20,55 @@ function Home(){
     };
 
     const deleteNote = (id) => {
-        api.delete(`/api/notes/delete/${id}/`).then((res) => {
+        api
+            .delete(`/api/notes/delete/${id}/`).then((res) => {
             if (res.status ===204) alert("Note deleted!");
             else alert ("Failed to delete Note");
+        
+         })
+            .catch((err) => alert(err));
         getNotes();
-        })
+
     }
+
+    const createNote = () => {
+        e.preventDefault()
+        api.post("/api/notes/", {content, title}).then(res => {
+            if (res.status === 201) alert("Note created!");
+            else alert("Failed to create Note");
+        }).catch(err => alert(err));
+        getNotes();
+        
 
     
 
-    return <div>Home</div>
+    return <div>
+        <div>
+            <h2>Notes</h2>
+        </div>
+        <h2>Create a Note</h2> 
+        <form onSubmit ={createNote}>
+            <lable htmlfor ="title">Title:</lable>
+            <br/>
+            <input 
+                type ="text" 
+                id="title" 
+                name="title" 
+                onChange={(e) => setTitle(e.target.value)} 
+                required 
+                value = {title}/>
+            <label htmlFor ="content">Content;</label>
+            <br />
+            <textarea 
+                id="content" 
+                name="content" 
+                onChange={(e) => setContent(e.target.value)} 
+                required 
+                value = {content}/>
+            <br/>
+            <input type ="submit" value="Create Note"/>
+        </form>
+    </div>
 }
 
 export default Home;
